7e59adad08439334161f29efc0e754e872eaa470
Michael Gaffney <michaelgaffney@Michaels-MacBook-Air.local>
2022-04-03 10:29:43 -0400

    Added updated readme, added datasets, wrote initial EDA r scipt file to explore the data.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BihariGaffneyOakley_ENV872_EDA_FinalProject
 Final project for Environmental Data Analytics using Coweeta LTER data
+
+Analysis by Eni Bihari, Michael Gaffney, and Cal Oakley
+
+Data and some scripts provided courtesy of Dean Urban
---
+++ Scripts/Coweeta_EDA.R
@@ -0,0 +1,33 @@
+#Coweeta data Initial EDA
+#Enikoe Bihari, Michael Gaffney, Cal Oakley
+
+#set working directory
+#setwd("/Users/michaelgaffney/Documents/Duke University/Nicholas School of the Environment/05 Spring 2022/ENVIRON 872 Environmental Data Analytics/EDA_FinalProject")
+
+#load packages
+library(tidyverse)
+library(readxl)
+library(sf)
+library(mapview)
+
+#load the species data for 108 coweeta plots (rare species already thinned)
+coweeta.species <- read_excel("./Data/Raw/chl_sppdataBA.xlsx", sheet = "dataBA")
+#load the environmental variables for the same 108 plots
+coweeta.env <- read_excel("./Data/Raw/chl_sitedata.xlsx")
+#load the geospatial data for the sites (sheet CHL has the sites)
+coweeta.xy <- read_excel("./Data/Raw/chl_XYdata.xlsx", sheet = "CHL")
+
+#sweet, let's get the XY data into a spatial format; UTM 17N is the projection
+coweeta.sf <- coweeta.xy %>% 
+  st_as_sf(coords = c('UTME','UTMN'),
+           crs=32617)
+#check out where this stuff shows up on the map
+mapView(coweeta.sf, col.regions = "darkgreen", map.types = "CartoDB.Positron", legend = FALSE)
+
+#epxlore the species data a little; try a random plot of some of the species (Acer Rubrum, Rhododendron maximum, Carya tomentosa) against elevation
+#set ggplot data source to null because we're using two different datasets
+ggplot(NULL) +
+  geom_point(aes(x = coweeta.env$Elevation, y = coweeta.species$ACRU), color = "darkgreen") +
+  geom_point(aes(x = coweeta.env$Elevation, y = coweeta.species$RHMA), color = "darkred") +
+  geom_point(aes(x = coweeta.env$Elevation, y = coweeta.species$CATO), color = "darkblue") 
+  
